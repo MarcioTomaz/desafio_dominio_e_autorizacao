5570a12a0987c2f64e682ed97ba12a861e9e965d
MarcioTomaz <marciotmaz@gmail.com>
2022-07-29 14:09:55 -0300

    Fix: seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,7 +14,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 
 INSERT INTO tb_genre ( id, name ) VALUES (1, 'Fantasia');
 INSERT INTO tb_genre ( id, name ) VALUES (2, 'Escolar');
-INSERT INTO tb_genre ( id, name ) VALUES (3, 'Terror');
-INSERT INTO tb_genre ( id, name ) VALUES (4, 'Futebol');
+INSERT INTO tb_genre ( id, name ) VALUES (3, 'pos-apocaliptico');
 
-INSERT INTO tb_movie ( id, title, subTitle, year, )
+INSERT INTO tb_movie ( id, genre_id, title, sub_Title, year, img_Uri, synopsis ) VALUES (1,1 ,'Yugioh', 'jogo de cartinha', 1998, 'www.google.com', 'mto doido');
+INSERT INTO tb_movie ( id, genre_id, title, sub_Title, year, img_Uri, synopsis ) VALUES (2,2 ,'Yugioh GX', 'jogo de cartinha na escola', 2004, 'www.google.com', 'mto doido na escola');
+INSERT INTO tb_movie ( id, genre_id, title, sub_Title, year, img_Uri, synopsis ) VALUES (3,3 ,'Yugioh 5DS', 'jogo de cartinha na cidade destruida', 2008, 'www.google.com', 'mto doido na cidade destruida');
